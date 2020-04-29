b836f577f0d84663fe6c3c2fe72ae297fc1cdd13
Helco <hermann.noll@hotmail.com>
2020-04-29 12:50:44 +0200

    Add additional frames test

--- test/test_multiple_frames.cpp
+++ test/test_multiple_frames.cpp
@@ -5,7 +5,7 @@ class MultipleFrames : public ::testing::Test {};
 TEST_F(MultipleFrames, WithoutDefaultImage)
 {
     // only tests meta behaviour, content is tested elsewhere
-    upng_t* upng = upng_new_from_file("test/resources/025.png");
+    upng_t* upng = upng_new_from_file("test/resources/excors/025.png");
     ASSERT_NE(nullptr, upng);
     ASSERT_EQ(UPNG_EOK, upng_header(upng));
     ASSERT_EQ(0, upng_get_plays(upng));
@@ -38,6 +38,23 @@ TEST_F(MultipleFrames, WithoutDefaultImage)
     ASSERT_EQ(3, upng_get_frame_index(upng));
     upng_get_frame_rect(upng, &curRect);
     ASSERT_EQ(defaultRect, curRect);
+
+    upng_free(upng);
+}
+
+TEST_F(MultipleFrames, FramesWrapAround)
+{
+    upng_t* upng = upng_new_from_file("test/resources/excors/025.png");
+    ASSERT_NE(nullptr, upng);
+    ASSERT_EQ(UPNG_EOK, upng_header(upng));
+    ASSERT_EQ(4, upng_get_frame_count(upng));
+
+    ASSERT_EQ(UPNG_EOK, upng_decode_next_frame(upng));
+    ASSERT_EQ(UPNG_EOK, upng_decode_next_frame(upng));
+    ASSERT_EQ(UPNG_EOK, upng_decode_next_frame(upng));
+    ASSERT_EQ(UPNG_EOK, upng_decode_next_frame(upng));
+    ASSERT_EQ(UPNG_EOK, upng_decode_next_frame(upng));
+    ASSERT_EQ(0, upng_get_frame_index(upng));
 
     upng_free(upng);
 }
